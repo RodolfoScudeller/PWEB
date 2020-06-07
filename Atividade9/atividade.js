90afb003693d6fea3535f0e3ee2fdb5478040ce9
RodolfoScudeller <60948686+RodolfoScudeller@users.noreply.github.com>
2020-06-07 18:13:30 -0300

    Add files via upload

---
+++ Atividade9/atividade.js
@@ -0,0 +1,9 @@
+function toUpper() {
+  var texto = document.getElementById('texto').value;
+  document.getElementById('texto').value = texto.toUpperCase();
+}
+
+function toLower() {
+  var texto = document.getElementById('texto').value;
+  document.getElementById('texto').value = texto.toLowerCase(); 
+}
